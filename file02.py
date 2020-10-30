4cd969aa7c897d80bb469fded710f70faaa222cf
Mitsuo <24877574+rkkr172@users.noreply.github.com>
2020-10-30 15:52:01 +0530

    Create file02.py

---
+++ file02.py
@@ -0,0 +1,11 @@
+
+print("Welcome to India".find("l"))
+print("Welcome to India".find("o",5,10)) 
+# start-5th, end-10th place
+
+print("Hello Raja Text".split())
+print("July-Aug XYZ".split())
+print("2*2=4".split("*"))
+
+print("\tTAB")
+print(r"\tTAB") # raw pringting
