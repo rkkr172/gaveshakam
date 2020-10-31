80dfa4864ee6a5e4829b8730dab7748e9b79b9c6
Mitsuo <24877574+rkkr172@users.noreply.github.com>
2020-10-31 18:54:57 +0530

    Update file02.py

--- file02.py
+++ file02.py
@@ -9,3 +9,16 @@ print("2*2=4".split("*"))
 
 print("\tTAB")
 print(r"\tTAB") # raw pringting
+
+#import re # part13
+#print(re.findall("[06-9]","1+2==3"))
+#print(re.findall(^"[0-9]","1+2==3"))
+
+# Vid_13
+print(re.findall(r'[0-9]','1+2==3'))
+print(re.findall(r'[a-cA-Z0-5]','Barbeque Nation Cc249'))
+
+# Vid_14
+print(re.findall(r"[0-9]","Mr Tyagi 8943"))
+print(re.findall(r"[A-Z]","Mr KumarM 1264"))
+print(re.findall(r"[0-9]","9-6==3"))
